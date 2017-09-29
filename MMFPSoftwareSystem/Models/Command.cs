fc73ba6a52234a9fabfdcab52a106bf927442dcd
tsurrdurr <tsurrdurr@users.noreply.github.com>
2017-09-29 19:59:11 +0300

    Создан проект Visual Studio. Создана основа для WPF MVVM проекта.

---
+++ MMFPSoftwareSystem/Models/Command.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MMFPSoftwareSystem
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private bool _canExecute = true;
+        private Action<object> _action;
+
+        public Command(Action action, bool canExecute = true)
+        {
+            _action = (x) => action();
+            _canExecute = canExecute;
+        }
+        public Command(Action<object> action, bool canExecute = true)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute
+        {
+            get { return _canExecute; }
+            set
+            {
+                if (_canExecute != value)
+                {
+                    _canExecute = value;
+                    OnCanExecuteChanged();
+                }
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke(parameter);
+        }
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ MMFPSoftwareSystem/ViewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMFPSoftwareSystem
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+
+        public MainViewModel()
+        {
+
+        }
+
+
+        public Command SampleCommand => _sampleCommand ?? (_sampleCommand = new Command(SampleMethod));
+
+
+        public string Test
+        {
+            get => "test";
+            set { OnPropertyChanged(nameof(Test)); }
+        }
+
+
+        private Command _sampleCommand;
+
+
+        private void SampleMethod()
+        {
+            Debug.Write("method");
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+
+        }
+        #endregion
+    }
+}
