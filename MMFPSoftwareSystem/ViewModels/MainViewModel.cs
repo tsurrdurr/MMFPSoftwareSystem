bb8d88df8749aa50d2c3da950913468138daab42
tsurrdurr <tsurrdurr@users.noreply.github.com>
2017-10-01 02:25:07 +0300

    Созданы виды и вью-модели частей программы.

--- MMFPSoftwareSystem/ViewModels/MainViewModel.cs
+++ MMFPSoftwareSystem/ViewModels/MainViewModel.cs
@@ -5,6 +5,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using MMFPSoftwareSystem.Views;
 
 namespace MMFPSoftwareSystem
 {
@@ -14,11 +16,37 @@ namespace MMFPSoftwareSystem
 
         public MainViewModel()
         {
-
+            
         }
 
-
+        public ModelingControlsViewModel modelingControlsViewModel => _modelingControlsViewModel ?? (_modelingControlsViewModel = new ModelingControlsViewModel());
+        public GraphViewModel graphViewModel => _graphViewModel ?? (_graphViewModel = new GraphViewModel());
+        public TheoryViewModel theoryViewModel => _theoryViewModel ?? (_theoryViewModel = new TheoryViewModel());
+        public TestingViewModel testingViewModel => _testingViewModel ?? (_testingViewModel = new TestingViewModel());
+        
         public Command SampleCommand => _sampleCommand ?? (_sampleCommand = new Command(SampleMethod));
+        public Command OpenAdminCommand => _openAdminCommand ?? (_openAdminCommand = new Command(OpenAdmin));
+        public Command OpenHelpCommand => _openHelpCommand ?? (_openHelpCommand = new Command(OpenHelp));
+        
+        private void OpenAdmin()
+        {
+            var adminWindow = new AdminView
+            {
+                DataContext = _adminViewModel ?? (_adminViewModel = new AdminViewModel()),
+                Owner = Application.Current.MainWindow
+            };
+            adminWindow.Show();
+        }
+
+        private void OpenHelp()
+        {
+            var helpWindow = new HelpView
+            {
+                DataContext = _helpViewModel ?? (_helpViewModel = new HelpViewModel()),
+                Owner = Application.Current.MainWindow
+            };
+            helpWindow.Show();
+        }
 
 
         public string Test
@@ -28,8 +56,16 @@ namespace MMFPSoftwareSystem
         }
 
 
-        private Command _sampleCommand;
+        private ModelingControlsViewModel _modelingControlsViewModel;
+        private GraphViewModel _graphViewModel;
+        private TheoryViewModel _theoryViewModel;
+        private TestingViewModel _testingViewModel;
+        private AdminViewModel _adminViewModel;
+        private HelpViewModel _helpViewModel;
 
+        private Command _sampleCommand;
+        private Command _openAdminCommand;
+        private Command _openHelpCommand;
 
         private void SampleMethod()
         {
---
+++ MMFPSoftwareSystem/Views/AdminView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MMFPSoftwareSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for AdminView.xaml
+    /// </summary>
+    public partial class AdminView : Window
+    {
+        public AdminView()
+        {
+            InitializeComponent();
+            this.Loaded += OnLoaded;
+            this.Closed += OnClosed;
+        }
+
+        private void OnLoaded(object sender, EventArgs eventArgs)
+        {
+            this.Owner.IsEnabled = false;
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            this.Owner.IsEnabled = true;
+        }
+    }
+}
