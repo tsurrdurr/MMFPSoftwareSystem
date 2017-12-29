316bd445c1801267617a698857dbf781612097b9
ryakupov <r.yakupov@pflb.ru>
2017-12-29 18:33:23 +0300

    забытые файлы

---
+++ MMFPSoftwareSystem/Helpers/TestChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MMFPCommonDataStructures;
+
+namespace MMFPSoftwareSystem.Helpers
+{
+    class TestChecker
+    {
+        /*
+         *  Выполняет проверку сета вопросов. Возвращает процент правильных ответов.
+         */
+        public static int checkTest(QuestionSet questionSet)
+        {
+            int correctAnswerCount = 0;
+            int totalQuestionsCount = 0;
+            foreach (var questionSection in questionSet.Sections)
+            {
+                foreach (var question in questionSection.Questions)
+                {
+                    totalQuestionsCount++;
+                    if (question.IsCorrect())
+                    {
+                        correctAnswerCount++;
+                        (question.Answers as ObservableCollection<Answer>)[question.SelectedAnswer ?? 0].IsCorrect = true;
+                    }
+                }
+            }
+            int result = (int)((float)correctAnswerCount / totalQuestionsCount * 100);
+            return result;
+        }
+    }
+}
